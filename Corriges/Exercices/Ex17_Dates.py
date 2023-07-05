f2317acc2eab9d24e25b2fef5989af6bc206f5f5
Twixy Twixy <twixy@free.fr>
2023-07-05 15:20:12 +0200

    exercice dates

--- Corriges/Exercices/Ex17_Dates.py
+++ Corriges/Exercices/Ex17_Dates.py
@@ -3,13 +3,13 @@
 from datetime import date
 
 def main():
-    aujoudhui = date.today()
-    avril = date(aujoudhui.year, 4, 1)
+    aujourdhui = date.today()
+    avril = date(aujourdhui.year, 4, 1)
 
-    if avril < aujoudhui: avril = date(aujoudhui.year + 1, 4, 1)
+    if avril < aujourdhui: avril = date(aujourdhui.year + 1, 4, 1)
 
     print(f"Date du prochain poisson d'avril : {avril.day:>02d}/{avril.month:>02d}/{avril.year}")
-    print(f"Nombre de jours avant le prochaine poisson d'avril : {(avril - aujoudhui).days}")
+    print(f"Nombre de jours avant le prochaine poisson d'avril : {(avril - aujourdhui).days}")
 
     print("_" * 40)
     input("Appuyer sur <Entrée> pour terminer !")
